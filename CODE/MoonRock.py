f321440bc9783cf7d51d97a33a96b325973b8ca6
gabrielbindi <binderbauer.gabriel@gmail.com>
2025-10-08 10:25:34 +0200

    08.10.2025

--- CODE/MoonRock.py
+++ CODE/MoonRock.py
@@ -7,15 +7,28 @@ pygame.display.set_caption("MoonRock")
 x = 400
 y = 550
 vel = 15
+bvel= 30
 
 #setup for Pygame
 screen = pygame.display.set_mode((800, 600))
 
-player_img = pygame.image.load('Player.png').convert_alpha()
+player_img = pygame.image.load('../PNGs/Player.png').convert_alpha()
 player_img = pygame.transform.scale(player_img, (50, 50))
-
-
+bullet_img = pygame.image.load('../PNGs/Laser Bullet.png').convert_alpha()
+bullet_img = pygame.transform.scale(bullet_img, (20, 20))
+bullet_img = pygame.transform.rotate(bullet_img, 90)
 running = True
+
+#Bullet_Class:
+#@param
+#@return
+class Projectile(object):
+    def __init__(self, x, y, width, color, facing):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.color = color
+        self.facing = facing
 
 while running:
     pygame.time.delay(100)
@@ -27,16 +40,15 @@ while running:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x >= 0:
         x -= vel
 
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x <= 750:
         x += vel
 
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_UP] and y >= 0:
         y -= vel
-
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_DOWN] and y <= 550:
         y += vel
 
     #if keys[pygame.K_SPACE]:
