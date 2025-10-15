c94d6909ae0f801a9306de94307714e59ad9fa2e
gabrielbindi <binderbauer.gabriel@gmail.com>
2025-10-15 11:03:04 +0200

    15.10.2025

--- CODE/MoonRock.py
+++ CODE/MoonRock.py
@@ -7,28 +7,18 @@ pygame.display.set_caption("MoonRock")
 x = 400
 y = 550
 vel = 15
-bvel= 30
+
 
 #setup for Pygame
 screen = pygame.display.set_mode((800, 600))
 
-player_img = pygame.image.load('../Assets/Player.png').convert_alpha()
+player_img = pygame.image.load('../moonrock/Assets/Player.png').convert_alpha()
 player_img = pygame.transform.scale(player_img, (50, 50))
-bullet_img = pygame.image.load('../Assets/Laser Bullet.png').convert_alpha()
-bullet_img = pygame.transform.scale(bullet_img, (20, 20))
+bullet_img = pygame.image.load('../moonrock/Assets/Laser Bullet.png').convert_alpha()
+bullet_img = pygame.transform.scale(bullet_img, (8, 16))
 bullet_img = pygame.transform.rotate(bullet_img, 90)
 running = True
 
-#Bullet_Class:
-#@param
-#@return
-class Projectile(object):
-    def __init__(self, x, y, width, color, facing):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.color = color
-        self.facing = facing
 
 while running:
     pygame.time.delay(100)
@@ -42,22 +32,29 @@ while running:
 
     if keys[pygame.K_LEFT] and x >= 0:
         x -= vel
-
     if keys[pygame.K_RIGHT] and x <= 750:
         x += vel
-
     if keys[pygame.K_UP] and y >= 0:
         y -= vel
-    if keys[pygame.K_DOWN] and y <= 550:
+    if keys[pygame.K_DOWN] and y <= 540:
         y += vel
 
-    #if keys[pygame.K_SPACE]:
-    #add bullet class
+    if keys[pygame.K_SPACE]:
+        class Bullet(pygame.sprite.Sprite):
+            def __init__(self, image, start_pos, speed_y):
+                super().__init__()
+                self.image = image
+                self.rect = self.image.get_rect(center=start_pos)
+                self.mask = pygame.mask.from_surface(self.image)
+                self.speed_y = speed_y
 
     screen.fill((0, 0, 0))
     screen.blit(player_img, (x, y))
     pygame.display.update()
     pygame.display.flip()
+
+
+
 
 pygame.quit()
 sys.exit()
