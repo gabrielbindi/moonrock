54e040072c97ac0aa642fd686d86dd7085607764
gabrielbindi <binderbauer.gabriel@gmail.com>
2025-10-20 07:46:01 +0200

    20.10.2025

--- CODE/MoonRock.py
+++ CODE/MoonRock.py
@@ -4,21 +4,43 @@ import sys
 pygame.init()
 pygame.display.set_caption("MoonRock")
 
-x = 400
-y = 550
+''' Variables '''
+
+player_x = 400
+player_y = 550
 vel = 15
+
+last_shot = 0
+shot_cooldown = 400
+
+bullet_group = pygame.sprite.Group()
 
 
 #setup for Pygame
 screen = pygame.display.set_mode((800, 600))
 
+''' Sprites '''
+
 player_img = pygame.image.load('../moonrock/Assets/Player.png').convert_alpha()
 player_img = pygame.transform.scale(player_img, (50, 50))
 bullet_img = pygame.image.load('../moonrock/Assets/Laser Bullet.png').convert_alpha()
 bullet_img = pygame.transform.scale(bullet_img, (8, 16))
-bullet_img = pygame.transform.rotate(bullet_img, 90)
+bullet_img = pygame.transform.rotate(bullet_img, 180)
 running = True
 
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, image, start_pos, speed_y):
+        super().__init__()
+        self.image = image
+        self.rect = self.image.get_rect(center=start_pos)
+        self.mask = pygame.mask.from_surface(self.image)
+        self.speed_y = speed_y
+
+    def update(self):
+        self.rect.y += self.speed_y
+        if self.rect.y < 0:
+            self.kill()
 
 while running:
     pygame.time.delay(100)
@@ -29,28 +51,27 @@ while running:
 
 
     keys = pygame.key.get_pressed()
+    current_time = pygame.time.get_ticks()
 
-    if keys[pygame.K_LEFT] and x >= 0:
-        x -= vel
-    if keys[pygame.K_RIGHT] and x <= 750:
-        x += vel
-    if keys[pygame.K_UP] and y >= 0:
-        y -= vel
-    if keys[pygame.K_DOWN] and y <= 540:
-        y += vel
+    if keys[pygame.K_LEFT] and player_x >= 0:
+        player_x -= vel
+    if keys[pygame.K_RIGHT] and player_x <= 750:
+        player_x += vel
+    if keys[pygame.K_UP] and player_y >= 0:
+        player_y -= vel
+    if keys[pygame.K_DOWN] and player_y <= 540:
+        player_y += vel
 
-    if keys[pygame.K_SPACE]:
-        class Bullet(pygame.sprite.Sprite):
-            def __init__(self, image, start_pos, speed_y):
-                super().__init__()
-                self.image = image
-                self.rect = self.image.get_rect(center=start_pos)
-                self.mask = pygame.mask.from_surface(self.image)
-                self.speed_y = speed_y
+    if keys[pygame.K_SPACE] and current_time - last_shot > shot_cooldown:
+        bullet = Bullet(bullet_img, (player_x, player_y), -10)
+        bullet_group.add(bullet)
+        last_shot = current_time
 
     screen.fill((0, 0, 0))
-    screen.blit(player_img, (x, y))
+    screen.blit(player_img, (player_x, player_y))
     pygame.display.update()
+    bullet_group.update()
+    bullet_group.draw(screen)
     pygame.display.flip()
 
 
